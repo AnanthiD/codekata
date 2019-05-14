8cabce292b380f59672332a2a30aadfbf41f425e
AnanthiD <50616589+AnanthiD@users.noreply.github.com>
2019-05-14 14:42:46 +0530

    Add files via upload

---
+++ guvi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number\n");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive\n");
+}
+elseif(n<0)
+{
+printf("negative\n");
+}
+else
+{
+printf("zero")
+}
+}
