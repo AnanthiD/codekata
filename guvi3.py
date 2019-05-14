bbe0eb60f6c360808106cc4b967b6229dceffdd4
AnanthiD <50616589+AnanthiD@users.noreply.github.com>
2019-05-14 15:27:57 +0530

    Add files via upload

---
+++ guvi3.py
@@ -0,0 +1,8 @@
+a2=input()
+if(a2.isalpha() and len(al)==1):
+  if(a2=='a' or a2=='e' or a2=='i' or a2=='o'or a2=='u'):
+      print("Vowel")
+  else:
+      print("Consonant")
+else:
+   print("invalid")
