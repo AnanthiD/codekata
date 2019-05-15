65b3041edfee7a8e6c69e74557b92e8a5642222e
AnanthiD <50616589+AnanthiD@users.noreply.github.com>
2019-05-15 13:58:05 +0530

    Add files via upload

---
+++ guvi10.py
@@ -0,0 +1,20 @@
+num=int(input())
+
+if(num>1):
+
+    for i in range(2,num):
+        
+	if((num%i)==0):
+            
+		print(num,"its is not a prime number")
+            
+		break
+
+    else:
+        
+	print(num, "num is a prime number")
+
+else:
+    
+	print("is not a prime number")
+            
