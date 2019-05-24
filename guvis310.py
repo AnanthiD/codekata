589db871e3449d37acbebb4e9c7cb2b4478f4d95
AnanthiD <50616589+AnanthiD@users.noreply.github.com>
2019-05-24 12:04:36 +0530

    Add files via upload

---
+++ guvis310.py
@@ -0,0 +1,16 @@
+a=input().split(" ")
+c=input().split(" ")
+d=int(a[0])
+e=int(a[1])
+f=int(c[0])
+g=int(c[1])
+d=(d*60)+e
+f=(f*60)+g
+if(d>f):
+    d=d-f
+else:
+    d=f-d
+z=d/60
+y=d%60
+print(int(z),end=" ")
+print(y)
