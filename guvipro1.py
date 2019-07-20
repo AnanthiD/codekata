334e610125314feeb1441932b13e55291bb17c89
AnanthiD <50616589+AnanthiD@users.noreply.github.com>
2019-07-20 14:24:59 +0530

    Add files via upload

---
+++ guvipro1.py
@@ -0,0 +1,12 @@
+n=int(input())
+lis=[]
+for k in range(n):
+	c=input()
+	lis.append(c)
+kar=[]
+for k in zip(*lis):
+	if(k.count(k[0])==len(k)):
+		kar.append(k[0])
+	else:
+		break
+print("".join(kar))
